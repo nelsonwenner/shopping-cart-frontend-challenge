06b7c51c148ef02fb39f46cb1c61dc96ca43b7fa
wenner <nelsondiasdemedeiros@gmail.com>
2021-03-22 11:20:00 -0300

    refactor: add components in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+
+import store from '@/store'
 
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from '@/styles/GlobalStyles'
 import light from '@/styles/themes/light'
+import Header from '@/components/Header'
+import Shopping from '@/pages/Shopping'
+
+import 'react-toastify/dist/ReactToastify.min.css'
 
 const App = () => (
-  <ThemeProvider theme={light}>
-    <GlobalStyles />
-    <h1>Hellow World!!</h1>
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={light}>
+      <GlobalStyles />
+      <ToastContainer newestOnTop />
+      <Header />
+      <Shopping />
+    </ThemeProvider>
+  </Provider>
 )
 
 export default App
